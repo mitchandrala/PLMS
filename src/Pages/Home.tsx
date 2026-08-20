0ccbf4a865cee43252571706be64e7a46bc6aca9
mitchandrala <mitchandrala05@gmail.com>
2026-08-20 15:07:07 +0530

    Installed react-router-dom & Implemented Navbar

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-160">
+      <div>
+        <h1 className="text-2xl">Welcome to PLMS</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "@mantine/core/styles.css";
 import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <MantineProvider>
+  <MantineProvider>
+    <BrowserRouter>
       <App />
-    </MantineProvider>
-  </StrictMode>,
+    </BrowserRouter>
+  </MantineProvider>,
 );
