e972c55a1a8af2d679e36e590949527801c7ebf4
mitchandrala <mitchandrala05@gmail.com>
2026-08-21 17:54:27 +0530

    Updated Slot Type And Implemented UI of Book Slot Form

--- src/Pages/BookSlot.tsx
+++ src/Pages/BookSlot.tsx
@@ -1,78 +1,130 @@
-import { Button, Checkbox, MaskInput, Select } from "@mantine/core";
-import { useState } from "react";
-import { type SlotType, type Slots } from "../Types/slotType";
+import { Button, Checkbox, Select, TextInput } from "@mantine/core";
+import {
+  type SlotType,
+  type SlotTypeAData,
+  type SlotTypeBData,
+  type SlotTypeCData,
+  type Slots,
+} from "../Types/slotType";
 import { DateTimePicker } from "@mantine/dates";
+import { useForm } from "@mantine/form";
+import { useState } from "react";
 
-const data: Slots = [
+// const data: Slots = [
+//   {
+//     slotName: "A1",
+//     isOccupied: false,
+//     support
+//   },
+//   {
+//     slotName: "A2",
+//     isOccupied: true,
+//   },
+//   {
+//     slotName: "A3",
+//     isOccupied: true,
+//   },
+//   {
+//     slotName: "A4",
+//     isOccupied: false,
+//   },
+//   {
+//     slotName: "A5",
+//     isOccupied: false,
+//   },
+//   {
+//     slotName: "B1",
+//     isOccupied: false,
+//   },
+//   {
+//     slotName: "B2",
+//     isOccupied: false,
+//   },
+//   {
+//     slotName: "B3",
+//     isOccupied: true,
+//   },
+//   {
+//     slotName: "B4",
+//     isOccupied: true,
+//   },
+//   {
+//     slotName: "B5",
+//     isOccupied: true,
+//   },
+//   {
+//     slotName: "C1",
+//     isOccupied: false,
+//   },
+//   {
+//     slotName: "C2",
+//     isOccupied: true,
+//   },
+//   {
+//     slotName: "C3",
+//     isOccupied: true,
+//   },
+//   {
+//     slotName: "C4",
+//     isOccupied: true,
+//   },
+//   {
+//     slotName: "C5",
+//     isOccupied: false,
+//   },
+// ];
+
+const dataA: SlotTypeAData[] = [
   {
     slotName: "A1",
-    isOccupied: false,
-  },
-  {
-    slotName: "A2",
     isOccupied: true,
   },
-  {
-    slotName: "A3",
-    isOccupied: true,
-  },
-  {
-    slotName: "A4",
-    isOccupied: false,
-  },
-  {
-    slotName: "A5",
-    isOccupied: false,
-  },
+];
+
+const dataB: SlotTypeBData[] = [
   {
     slotName: "B1",
     isOccupied: false,
   },
-  {
-    slotName: "B2",
-    isOccupied: false,
-  },
-  {
-    slotName: "B3",
-    isOccupied: true,
-  },
-  {
-    slotName: "B4",
-    isOccupied: true,
-  },
-  {
-    slotName: "B5",
-    isOccupied: true,
-  },
+];
+
+const dataC: SlotTypeCData[] = [
   {
     slotName: "C1",
-    isOccupied: false,
-  },
-  {
-    slotName: "C2",
     isOccupied: true,
-  },
-  {
-    slotName: "C3",
-    isOccupied: true,
-  },
-  {
-    slotName: "C4",
-    isOccupied: true,
-  },
-  {
-    slotName: "C5",
-    isOccupied: false,
   },
 ];
 
 const BookSlot = () => {
-  // const [formData, setFormData] = useState({});
-  const [vehicleNumber, setVehicleNumber] = useState<string>("");
-  const [vehicleType, setVehicleType] = useState<string | null>("");
   const [checked, setChecked] = useState<boolean>(true);
-  const [slotNumber, setSlotNumber] = useState<string | null>("");
-  const [entryTime, setEntrytTime] = useState<string | null>("");
+
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      slotName: "",
+      vehicleNumber: "",
+      vehicleType: "",
+      isChecked: checked,
+      entryTime: "",
+    },
+    validate: {
+      vehicleNumber: (value) => {
+        if (value.length < 9) return "Enter valid vehicle number";
+        if (value.length >= 9) {
+          return /^[A-Z]{2}[0-9]{2}[A-Z]{1,2}[0-9]{4}$/i.test(value)
+            ? null
+            : "Invalid vehicle number";
+        }
+      },
+      vehicleType: (value) =>
+        value.length < 1 ? "Vehicle Type is required" : null,
+      entryTime: (value) =>
+        value.length < 1 ? "Entry Time is required" : null,
+      slotName: (value: string) =>
+        checked === false &&
+        (value.length < 1 ? "Slot Name is required" : null),
+    },
+  });
 
   const selectSlotData = data
     .filter((val: SlotType) => {
@@ -82,65 +134,89 @@ const BookSlot = () => {
     })
     .map((val: SlotType) => val.slotName);
 
-  console.log(selectSlotData);
-  console.log(new Date());
-
-  const date = new Date();
-  console.log(date.getFullYear(), date.getDay());
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    console.log(vehicleNumber);
-    console.log(vehicleType);
-    console.log(checked);
-    console.log(slotNumber);
-    console.log(entryTime);
+  const handleSubmit = async (values: typeof form.values) => {
+    console.log(values);
+    form.reset();
+    setChecked(true);
   };
+
   return (
-    <div className="px-10 py-10">
-      <h1 className="text-xl font-semibold mb-7">Book Slot</h1>
-      <form onSubmit={onSubmit} className="w-50 flex flex-col gap-5">
-        <MaskInput
+    <div className="max-h-screen h-170 w-full flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center bg-blue-100 max-h-full py-8 w-75 rounded-2xl shadow-lg ">
+        <h1 className="text-xl font-semibold mb-9">Book Slot</h1>
+        <form
+          onSubmit={form.onSubmit(handleSubmit)}
+          className="w-50 flex flex-col gap-5"
+        >
+          {/* <MaskInput
           required
-          className="w-50"
           label="Vehicle No."
           mask="AA 99 AA 9999"
+          // key={form.key("vehicleNumber")}
           placeholder="__ __ __ ____"
-          onChangeRaw={(val) => setVehicleNumber(val)}
-        />
-        <Select
-          value={vehicleType}
-          required
-          label="Vehicle Type"
-          placeholder="Vehicle Type"
-          data={["BIKE", "CAR", "SUV"]}
-          onChange={(val) => setVehicleType(val)}
-        />
-        <Checkbox
-          label="Automatic Slot Assign"
-          checked={checked}
-          onChange={(e) => setChecked(e.target.checked)}
-        />
-        {checked === false && (
+          defaultValue={String(form.getValues().vehicleNumber)}
+          onChangeRaw={(raw) =>
+            form.setFieldValue("vehicleNumber", raw, { forceUpdate: false })
+          }
+          className="w-50"
+          error={
+            form.getValues().vehicleNumber.length < 1 &&
+            "Invalid Vehicle Number"
+          }
+        /> */}
+
+          <TextInput
+            required
+            label="Vehicle No."
+            placeholder="Vehicle number"
+            key={form.key("vehicleNumber")}
+            {...form.getInputProps("vehicleNumber")}
+            className="w-50"
+          />
+
           <Select
             required
-            label="Slot No."
-            placeholder="Select Slot No."
-            data={selectSlotData}
-            onChange={(val) => setSlotNumber(val)}
+            label="Vehicle Type"
+            placeholder="Select Vehicle type"
+            data={["BIKE", "CAR", "SUV"]}
+            key={form.key("vehicleType")}
+            {...form.getInputProps("vehicleType")}
           />
-        )}
+          <Checkbox
+            label="Automatic Slot Assign"
+            key={form.key("isChecked")}
+            {...form.getInputProps("isChecked", { type: "checkbox" })}
+            onClick={() => {
+              if (checked === false) {
+                form.setFieldValue("slotName", "");
+              }
+              setChecked((val) => !val);
+            }}
+          />
 
-        <DateTimePicker
-          required
-          className="w-50"
-          label="Pick date and time"
-          defaultValue={new Date()}
-          onChange={(value) => setEntrytTime(value)}
-        />
+          {checked === false && (
+            <Select
+              required
+              label="Slot Name."
+              placeholder="Select Slot name"
+              data={selectSlotData}
+              key={form.key("slotName")}
+              {...form.getInputProps("slotName")}
+            />
+          )}
 
-        <Button type="submit">Book Slot</Button>
-      </form>
+          <DateTimePicker
+            required
+            className="w-50"
+            label="Entry Time"
+            placeholder="Date with Time"
+            key={form.key("entryTime")}
+            {...form.getInputProps("entryTime")}
+          />
+
+          <Button type="submit">Book Slot</Button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/Types/slotType.ts
+++ src/Types/slotType.ts
@@ -17,21 +17,44 @@ export type SlotName =
   | "C4"
   | "C5";
 
+type SlotA = "BIKE" | "CAR";
+type SlotB = "CAR" | "SUV";
+type SlotC = VehicleType;
+
 export type VehicleDetails = {
   vehicleNumber: string;
   vehicleType: VehicleType;
 };
 
-export type SlotType = {
-  slotName: SlotName;
+export type SlotType<T, K> = {
+  slotName: K;
   isOccupied: boolean;
+  supportVehicleType?: T;
   vehicleDetails?: VehicleDetails;
   charge?: number;
   entryTime?: Date;
   exitTime?: Date;
 };
 
-export type Slots = SlotType[];
+export type SlotTypeA = Extract<SlotName, "A1" | "A2" | "A3" | "A4" | "A5">;
+export type SlotTypeB = Extract<SlotName, "B1" | "B2" | "B3" | "B4" | "B5">;
+export type SlotTypeC = Extract<SlotName, "C1" | "C2" | "C3" | "C4" | "C5">;
+
+// export type Slots = SlotType[];
+
+export type SlotTypeAData = SlotType<SlotA, SlotTypeA>;
+export type SlotTypeBData = SlotType<SlotB, SlotTypeB>;
+export type SlotTypeCData = SlotType<SlotC, SlotTypeC>;
+
+export type Slots = SlotTypeAData[] & SlotTypeBData[] & SlotTypeCData[];
+
+export type InitialValue = {
+  slotName: SlotName | string;
+  vehicleNumber: string;
+  vehicleType: VehicleType | string;
+  isChecked: boolean;
+  entryTime: string;
+};
 
 // export type Slots = [
 //   A1: SlotType,
@@ -55,22 +78,4 @@ export type Slots = SlotType[];
 //   BIKE = "BIKE",
 //   CAR = "CAR",
 //   SUV = "SUV",
-// }
-
-// export enum SlotName {
-//   A1 = "A1",
-//   A2 = "A2",
-//   A3 = "A3",
-//   A4 = "A4",
-//   A5 = "A5",
-//   B1 = "B1",
-//   B2 = "B2",
-//   B3 = "B3",
-//   B4 = "B4",
-//   B5 = "B5",
-//   C1 = "C1",
-//   C2 = "C2",
-//   C3 = "C3",
-//   C4 = "C4",
-//   C5 = "C5",
 // }
