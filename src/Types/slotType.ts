9e400420d088a8b0d2488bb69e45dc6ad324c3fe
mitchandrala <mitchandrala05@gmail.com>
2026-08-26 12:09:07 +0530

    Implement Logic of Search and Filter by vehicle no. , vehicle type & slot status

--- src/Types/slotType.ts
+++ src/Types/slotType.ts
@@ -47,65 +47,9 @@ export type FormData = {
 
 export type TotalVehicle = Record<VehicleType, number>;
 
-// type SlotA = "BIKE" | "CAR";
-// type SlotB = "CAR" | "SUV";
-// type SlotC = VehicleType;
+export type Status = "AVAILABLE" | "OCCUPIED";
 
-// export type VehicleDetails<T> = {
-//   vehicleNumber: string;
-//   vehicleType: T;
-// };
-
-// export type SlotType<T, K> = {
-//   slotName: K;
-//   isOccupied: boolean;
-//   supportVehicleTypesupportVehicleType: T[];
-//   vehicleDetails?: VehicleDetails<T>;
-//   charge?: number;
-//   entryTime?: Date;
-//   exitTime?: Date;
-// };
-
-// export type SlotTypeA = Extract<SlotName, "A1" | "A2" | "A3" | "A4" | "A5">;
-// export type SlotTypeB = Extract<SlotName, "B1" | "B2" | "B3" | "B4" | "B5">;
-// export type SlotTypeC = Extract<SlotName, "C1" | "C2" | "C3" | "C4" | "C5">;
-
-// // export type Slots = SlotType[];
-
-// export type SlotTypeAData = SlotType<SlotA, SlotTypeA>[];
-// export type SlotTypeBData = SlotType<SlotB, SlotTypeB>[];
-// export type SlotTypeCData = SlotType<SlotC, SlotTypeC>[];
-
-// export type Slots = SlotTypeAData & SlotTypeBData & SlotTypeCData;
-
-// export type InitialValue = {
-//   slotName: SlotName | string;
-//   vehicleNumber: string;
-//   vehicleType: VehicleType | string;
-//   isChecked: boolean;
-//   entryTime: string;
-// };
-
-// export type Slots = [
-//   A1: SlotType,
-//   A2: SlotType,
-//   A3: SlotType,
-//   A4: SlotType,
-//   A5: SlotType,
-//   B1: SlotType,
-//   B2: SlotType,
-//   B3: SlotType,
-//   B4: SlotType,
-//   B5: SlotType,
-//   C1: SlotType,
-//   C2: SlotType,
-//   C3: SlotType,
-//   C4: SlotType,
-//   C5: SlotType,
-// ];
-
-// export enum VehicleType {
-//   BIKE = "BIKE",
-//   CAR = "CAR",
-//   SUV = "SUV",
-// }
+export type ActiveSlotTable = {
+  slotName: SlotName;
+  slotStatus: Status;
+} & Partial<Omit<ActiveSlot, "slotName">>;
